d1b2373b7e59366a75f6939fef42477719c3d9a5
Dom <domeccleston@gmail.com>
2025-06-28 11:25:16 +0100

    add map

---
+++ app/map/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { createClient } from "@/utils/supabase/client";
+import dynamic from "next/dynamic";
+import "leaflet/dist/leaflet.css";
+
+// Dynamically import MapContainer to avoid SSR issues
+const MapContainer = dynamic(
+  () => import("react-leaflet").then((mod) => mod.MapContainer),
+  { ssr: false }
+);
+
+const TileLayer = dynamic(
+  () => import("react-leaflet").then((mod) => mod.TileLayer),
+  { ssr: false }
+);
+
+export default function MapPage() {
+  const supabase = createClient();
+
+  return (
+    <main className="h-screen w-full">
+      <MapContainer
+        center={[51.505, -0.09]} // London coordinates
+        zoom={13}
+        className="h-full w-full"
+        zoomControl={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </main>
+  );
+}
