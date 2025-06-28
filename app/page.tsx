f9e9f35f056e1f10794b70410186a3cbeed27e8c
Dom <domeccleston@gmail.com>
2025-06-28 11:02:25 +0100

    add supabase sd

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
+import { createClient } from "@/utils/supabase/client";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = createClient();
+
+  console.log(supabase);
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
